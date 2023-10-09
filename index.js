13d0419913748c4703e0844de41ac516c6cb1775
Bruno Bortagaray <bruno.bortagaray@highsnobiety.com>
2023-10-09 15:59:07 +0200

    fix: Removed the thumbnail variable

--- index.js
+++ index.js
@@ -1,98 +1,81 @@
-const carousel = document.querySelector('.carousel');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
+const carousel = document.querySelector(".carousel");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
 let currentIndex = 0;
 
 // Function to update the current slide
 function updateCurrentSlide(index) {
-    const items = document.querySelectorAll('.carousel-item');
-    items.forEach((item, i) => {
-        if (i === index) {
-            item.classList.add('current');
-        } else {
-            item.classList.remove('current');
-        }
-    });
-
-    // Disable prevButton on the first slide
-    if (index === 0) {
-        prevButton.setAttribute('disabled', 'true');
+  const items = document.querySelectorAll(".carousel-item");
+  items.forEach((item, i) => {
+    if (i === index) {
+      item.classList.add("current");
     } else {
-        prevButton.removeAttribute('disabled');
+      item.classList.remove("current");
     }
+  });
 
-    // Disable nextButton on the last slide
-    if (index === items.length - 1) {
-        nextButton.setAttribute('disabled', 'true');
-    } else {
-        nextButton.removeAttribute('disabled');
-    }
+  // Disable prevButton on the first slide
+  if (index === 0) {
+    prevButton.setAttribute("disabled", "true");
+  } else {
+    prevButton.removeAttribute("disabled");
+  }
+
+  // Disable nextButton on the last slide
+  if (index === items.length - 1) {
+    nextButton.setAttribute("disabled", "true");
+  } else {
+    nextButton.removeAttribute("disabled");
+  }
 }
 
-prevButton.addEventListener('click', () => {
-    currentIndex = currentIndex - 1;
-    if (currentIndex < 0) {
-        currentIndex = carousel.children.length - 1;
-    }
-    scrollToItem(currentIndex);
-    updateCurrentSlide(currentIndex);
+prevButton.addEventListener("click", () => {
+  currentIndex = currentIndex - 1;
+  if (currentIndex < 0) {
+    currentIndex = carousel.children.length - 1;
+  }
+  scrollToItem(currentIndex);
+  updateCurrentSlide(currentIndex);
 });
 
-nextButton.addEventListener('click', () => {
-    currentIndex = currentIndex + 1;
-    if (currentIndex >= carousel.children.length) {
-        currentIndex = 0;
-    }
-    scrollToItem(currentIndex);
-    updateCurrentSlide(currentIndex);
+nextButton.addEventListener("click", () => {
+  console.log("yay");
+  currentIndex = currentIndex + 1;
+  if (currentIndex >= carousel.children.length) {
+    currentIndex = 0;
+  }
+  scrollToItem(currentIndex);
+  updateCurrentSlide(currentIndex);
 });
 
 // Keyboard navigation
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowLeft') {
-        currentIndex = currentIndex - 1;
-        if (currentIndex < 0) {
-            currentIndex = carousel.children.length - 1;
-        }
-        scrollToItem(currentIndex);
-        updateCurrentSlide(currentIndex);
-    } else if (e.key === 'ArrowRight') {
-        currentIndex = currentIndex + 1;
-        if (currentIndex >= carousel.children.length) {
-            currentIndex = 0;
-        }
-        scrollToItem(currentIndex);
-        updateCurrentSlide(currentIndex);
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    currentIndex = currentIndex - 1;
+    if (currentIndex < 0) {
+      currentIndex = carousel.children.length - 1;
     }
+    scrollToItem(currentIndex);
+    updateCurrentSlide(currentIndex);
+  } else if (e.key === "ArrowRight") {
+    currentIndex = currentIndex + 1;
+    if (currentIndex >= carousel.children.length) {
+      currentIndex = 0;
+    }
+    scrollToItem(currentIndex);
+    updateCurrentSlide(currentIndex);
+  }
 });
 
 function scrollToItem(index) {
-    const item = carousel.children[index];
-    const vw = window.innerWidth; // Get the viewport width
-    const scrollLeft = item.offsetLeft - (vw * 0.025); // Adjusted for 95vw
-    carousel.scrollTo({
-        left: scrollLeft,
-        behavior: 'smooth',
-    });
+  const item = carousel.children[index];
+  const vw = window.innerWidth; // Get the viewport width
+  const scrollLeft = item.offsetLeft - vw * 0.025; // Adjusted for 95vw
+  carousel.scrollTo({
+    left: scrollLeft,
+    behavior: "smooth",
+  });
 }
-
-
-// Event listeners for thumbnail clicks (click and touchend)
-thumbnails.forEach((thumbnail, index) => {
-    thumbnail.addEventListener('click', () => {
-        currentIndex = index;
-        updateSlider();
-        updateThumbnail();
-    });
-
-    thumbnail.addEventListener('touchend', () => {
-        currentIndex = index;
-        updateSlider();
-        updateThumbnail();
-    });
-});
-
 
 // Initialize the current slide
 updateCurrentSlide(currentIndex);
-
