54941e50f921d3a0e327908820dd7458c7d9f693
Zeina Saade <saadezeina@hotmail.com>
2023-10-09 15:15:56 +0200

    enable swipe gesture

--- painting.js
+++ painting.js
@@ -28,3 +28,17 @@ thumbnails.forEach((thumbnail, index) => {
     });
 });
 
+// Event listeners for thumbnail clicks (click and touchend)
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        currentIndex = index;
+        updateSlider();
+        updateThumbnail();
+    });
+
+    thumbnail.addEventListener('touchend', () => {
+        currentIndex = index;
+        updateSlider();
+        updateThumbnail();
+    });
+});
